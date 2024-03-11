c92ab9308a7b2a4a9941f9c54cb914af824f20b1
Mohamed Gamal <150333650+MohamedGamalabdelazizhasanin@users.noreply.github.com>
2024-03-11 15:33:14 -0700

    Add files via upload

---
+++ min.js
@@ -0,0 +1,27 @@
+
+let price = 100;
+let discount = truo;
+let discountAmount = 30;
+let countye = "egypt"
+
+
+if(discount === truo){
+    price -= discountAmount;
+}
+else if (discount ==="egypt"){
+    price -= 40;
+
+}
+console.log(price);
+
+let phone = {
+    name: 'mohamed gamal',
+    agee:45,
+    plice: 45*45,
+    fount:' من كَبِرَ في السِّنّ الرفيع الشأن سيِّد القوم المعلم والرئيس من أسماء الله الحسنى',
+
+   
+}
+
+
+
