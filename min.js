d41dfff402367a9bf9e80d02afec9935a8d2336d
mohamed <mg4545jkl@gmail.com>
2024-03-11 13:05:20 -0700

    new img and html and css and js filse

---
+++ min.js
@@ -0,0 +1,18 @@
+
+let price = 100;
+let discount = truo;
+let discountAmount = 30;
+let countye = "egypt"
+
+
+if(discount === truo){
+    price -= discountAmount;
+}
+else if (discount ==="egypt"){
+    price -= 40;
+
+}
+console.log(price);
+
+
+
